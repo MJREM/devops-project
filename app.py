3b667e5fd6e5cd0db165f37d8f0703c6051186f6
MJREM <mohammed@DESKTOP-CHGPNG6.localdomain>
2026-06-06 00:35:04 +0300

    Add GitHub Actions CI

--- app.py
+++ app.py
@@ -1,2 +1,34 @@
-print("Hello DevOps")
-print("version 2")
+from flask import Flask
+import psycopg2
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+
+    conn = psycopg2.connect(
+        host="db",
+        database="companydb",
+        user="admin",
+        password="admin123"
+    )
+
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM employees")
+
+    employees = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    html = "<h1>Employees</h1>"
+
+    for emp in employees:
+        html += f"<p>{emp[0]} - {emp[1]} - {emp[2]}</p>"
+
+    return html
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
